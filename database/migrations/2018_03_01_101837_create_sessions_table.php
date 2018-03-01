24b89e5ad1a1fefe20dea945e6b3d8f1749a4954
Dave Roverts <me@daveroverts.nl>
2018-03-01 12:00:32 +0100

    Adds a few migrations
    Mentors, sessions, sessionType, and students

--- database/migrations/2018_03_01_101837_create_sessions_table.php
+++ database/migrations/2018_03_01_101837_create_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('session', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('vatsim_id', 7);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('mentor')->unsigned()->index();
+            $table->integer('student')->unsigned()->index();
+            $table->dateTime('begin');
+            $table->dateTime('end');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('session');
     }
 }
--- database/migrations/2018_03_01_102943_create_type_session.php
+++ database/migrations/2018_03_01_102943_create_type_session.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTypeSession extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('type', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('vatsim_id', 7);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->text('typeSession');
         });
     }
 
@@ -31,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('type');
     }
 }
