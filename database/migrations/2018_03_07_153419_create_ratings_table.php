e01e0d75907297384e2acbf626342d3f41a8f085
Dave Roverts <me@daveroverts.nl>
2018-03-08 09:23:56 +0100

    Makes the array look more pretty

--- database/migrations/2018_03_07_153419_create_ratings_table.php
+++ database/migrations/2018_03_07_153419_create_ratings_table.php
@@ -21,16 +21,16 @@ class CreateRatingsTable extends Migration
 
         //Inserting all the types of sessions that are available
         $ratings = [
-            ['shortName' => 'OBS', 'longName' => "Observer"],
-            ['shortName' => 'S1', 'longName' => "Student"],
-            ['shortName' => 'S2', 'longName' => "Student 2"],
-            ['shortName' => 'S3', 'longName' => "Senior Student"],
-            ['shortName' => 'C1', 'longName' => "Controller"],
-            ['shortName' => 'C3', 'longName' => "Senior Controller"],
-            ['shortName' => 'I1', 'longName' => "Instructor"],
-            ['shortName' => 'I3', 'longName' => "Senior Instructor"],
-            ['shortName' => 'SUP', 'longName' => "Supervisor"],
-            ['shortName' => 'ADM', 'longName' => "Administrator"],
+            ['shortName' => 'OBS',  'longName' => "Observer"],
+            ['shortName' => 'S1',   'longName' => "Student"],
+            ['shortName' => 'S2',   'longName' => "Student 2"],
+            ['shortName' => 'S3',   'longName' => "Senior Student"],
+            ['shortName' => 'C1',   'longName' => "Controller"],
+            ['shortName' => 'C3',   'longName' => "Senior Controller"],
+            ['shortName' => 'I1',   'longName' => "Instructor"],
+            ['shortName' => 'I3',   'longName' => "Senior Instructor"],
+            ['shortName' => 'SUP',  'longName' => "Supervisor"],
+            ['shortName' => 'ADM',  'longName' => "Administrator"],
         ];
         DB::table('ratings')->insert($ratings);
     }
