5ae81659df2799cf93a625ef1a9941e8f3bfb1fd
Dave Roverts <me@daveroverts.nl>
2018-03-08 09:02:32 +0100

    Adds all the ratings

--- database/migrations/2018_03_07_153419_create_ratings_table.php
+++ database/migrations/2018_03_07_153419_create_ratings_table.php
@@ -16,7 +16,24 @@ class CreateRatingsTable extends Migration
         Schema::create('ratings', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('shortName');
+            $table->string('longName');
         });
+
+        //Inserting all the types of sessions that are available
+        $ratings = [
+            ['shortName' => 'OBS', 'longName' => "Observer"],
+            ['shortName' => 'S1', 'longName' => "Student"],
+            ['shortName' => 'S2', 'longName' => "Student 2"],
+            ['shortName' => 'S3', 'longName' => "Senior Student"],
+            ['shortName' => 'C1', 'longName' => "Controller"],
+            ['shortName' => 'C3', 'longName' => "Senior Controller"],
+            ['shortName' => 'I1', 'longName' => "Instructor"],
+            ['shortName' => 'I3', 'longName' => "Senior Instructor"],
+            ['shortName' => 'SUP', 'longName' => "Supervisor"],
+            ['shortName' => 'ADM', 'longName' => "Administrator"],
+        ];
+        DB::table('ratings')->insert($ratings);
     }
 
     /**
