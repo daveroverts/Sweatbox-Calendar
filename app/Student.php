6db016564c8532e1902e5593a932c3ed3b564100
Dave Roverts <me@daveroverts.nl>
2018-03-09 16:47:10 +0100

    Adds assignable attributes

--- app/Student.php
+++ app/Student.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'studentName', 'vatsim_id', 'email', 'rating', 'mentor',
+    ];
+
     function user(){
         return $this->belongsTo('App\User', 'id');
     }
