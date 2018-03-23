28ae17cfbdbd95d9d5f435c28ef5da4f0a96549b
Dave Roverts <me@daveroverts.nl>
2018-03-23 11:04:26 +0100

    Adds working overview + create view

---
+++ resources/views/mentor/create.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create new mentor</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('mentor.store') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address <i class="fa fa-envelope"></i></label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="vatsim_id" class="col-md-4 col-form-label text-md-right">Vatsim ID</label>
+
+                                <div class="col-md-6">
+                                    <input id="vatsim_id" type="text" class="form-control{{ $errors->has('vatsim_id') ? ' is-invalid' : '' }}" name="vatsim_id" value="{{ old('vatsim_id') }}" required>
+
+                                    @if ($errors->has('vatsim_id'))
+                                        <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('vatsim_id') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="rating" class="col-md-4 col-form-label text-md-right"><i class="fa fa-list-ul"></i> Rating</label>
+
+                                <div class="col-md-6">
+                                    <select class="custom-select" name="rating">
+                                        @foreach($ratings as $rating)
+                                            <option name="rating" value="{{$rating->id}}">{{ $rating->longName }} [{{ $rating->shortName }}]</option>
+                                        @endforeach
+                                    </select>
+
+
+
+                                    @if ($errors->has('rating'))
+                                        <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('rating') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Register
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/mentor/overview.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @if(Auth::check())
+            <h2>Mentors Overview</h2>
+            <a href="/mentor/create" class="btn btn-primary"><i class="fa fa-user-plus"></i> Add new Mentor</a>
+            <br><br>
+            <table class="table table-hover">
+                <thead><tr>
+                    <th>Vatsim ID</th>
+                    <th>Name</th>
+                    <th>E-Mail</th>
+                    <th>Rating</th>
+                    <th colspan="3">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse($mentors as $mentor)
+                        <tr>
+                            <td><a href="https://stats.vatsim.net/search_id.php?id={{ $mentor->vatsim_id }}">{{ $mentor->vatsim_id }}</a></td>
+                            <td>{{ $mentor->name }}</td>
+                            <td><a href="mailto:{{ $mentor->email }}">{{ $mentor->email }}</a></td>
+                            <td>{{ $mentor->rating->longName }} [{{ $mentor->rating->shortName }}]</td>
+
+                            <td><form action="/student/{{$mentor->id}}/edit">
+                                    <button class="btn btn-primary"><i class="fa fa-edit"></i> Edit Mentor</button>
+                                    @csrf
+                                </form></td>
+                        </tr>
+                        @empty
+                            <p>No Mentors are in the system, consider adding one, using the button above</p>
+                        @endforelse
+                </tbody>
+            </table>
+        @endif
+    </div>
+@endsection
