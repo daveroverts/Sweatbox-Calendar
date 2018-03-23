e12e210d251154888b633853c504cb1c7337b617
Dave Roverts <me@daveroverts.nl>
2018-03-23 10:26:13 +0100

    Removes register button from navbar, places it now in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,7 @@
 
                         <a href="calendar" class="btn btn-primary" role="button"><i class="fa fa-calendar"></i> Calendar Overview</a>
                         <a href="student" class="btn btn-primary" role="button"><i class="fa fa-users"></i> Students Overview</a>
+                            <a href="{{ route('register') }}" class="btn btn-primary" role="button"><i class="fa fa-user-plus"></i>Register new mentor</a>
                     </div>
                 </div>
             </div>
