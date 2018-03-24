dc596bd059895161d9c99b45a89df88bada9f5d5
Dave Roverts <me@daveroverts.nl>
2018-03-24 11:55:35 +0100

    Creates 2 different migrations for foreign keys

---
+++ database/migrations/2018_03_24_105344_create_sessions_foreign_keys.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('typeSession_id')->references('id')->on('session_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['student_id']);
+            $table->dropForeign(['typeSession_id']);
+        });
+    }
+}
--- database/migrations/2018_03_24_105355_create_students_foreign_keys.php
+++ database/migrations/2018_03_24_105355_create_students_foreign_keys.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateForeignKeys extends Migration
+class CreateStudentsForeignKeys extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,6 @@ class CreateForeignKeys extends Migration
      */
     public function up()
     {
-        Schema::table('sessions', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('student_id')->references('id')->on('students');
-            $table->foreign('typeSession_id')->references('id')->on('session_types');
-        });
-
         Schema::table('students', function (Blueprint $table) {
             $table->foreign('mentor_id')->references('id')->on('users');
             $table->foreign('rating_id')->references('id')->on('ratings');
@@ -32,12 +26,6 @@ class CreateForeignKeys extends Migration
      */
     public function down()
     {
-        Schema::table('sessions', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropForeign(['student_id']);
-            $table->dropForeign(['typeSession_id']);
-        });
-
         Schema::table('students', function (Blueprint $table) {
             $table->dropForeign(['mentor_id']);
             $table->dropForeign(['rating_id']);
