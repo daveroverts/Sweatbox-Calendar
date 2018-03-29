dc61219bb2134dae25d92d50fd9cb236f0c6d32d
Dave Roverts <me@daveroverts.nl>
2018-03-29 10:34:40 +0200

    Puts link to my GitHub page on the same line

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,8 +71,7 @@ $carbon = new \Carbon\Carbon();
             //An script to generate the copyright date using the server's year
             $fromYear = 2018;
             $thisYear = (int)date('Y');
-            echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> <a href="https://github.com/daveroverts/Sweatbox-Calendar/">Sweatbox Calendar</a>, created by
-            <a href="https://github.com/daveroverts/">Dave Roverts</a>.
+            echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> <a href="https://github.com/daveroverts/Sweatbox-Calendar/">Sweatbox Calendar</a>, created by <a href="https://github.com/daveroverts/">Dave Roverts</a>.
         </footer>
     </main>
 </div>
