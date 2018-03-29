5daddfc8979eec35939dff84a6f8861faeb2707f
Dave Roverts <me@daveroverts.nl>
2018-03-29 10:33:26 +0200

    Moves footer to be within <main>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,14 +67,14 @@ $carbon = new \Carbon\Carbon();
 
     <main class="py-4">
         @yield('content')
+        <footer><span class="fa fa-copyright"></span> <?php
+            //An script to generate the copyright date using the server's year
+            $fromYear = 2018;
+            $thisYear = (int)date('Y');
+            echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> <a href="https://github.com/daveroverts/Sweatbox-Calendar/">Sweatbox Calendar</a>, created by
+            <a href="https://github.com/daveroverts/">Dave Roverts</a>.
+        </footer>
     </main>
-    <footer><span class="fa fa-copyright"></span> <?php
-        //An script to generate the copyright date using the server's year
-        $fromYear = 2018;
-        $thisYear = (int)date('Y');
-        echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> <a href="https://github.com/daveroverts/Sweatbox-Calendar/">Sweatbox Calendar</a>, created by
-        <a href="https://github.com/daveroverts/">Dave Roverts</a>.
-    </footer>
 </div>
 
 <!-- Scripts -->
