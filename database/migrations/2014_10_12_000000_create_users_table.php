a3ef744aa6b7d58c938ee0b18d81442764ef0a08
Dave Roverts <me@daveroverts.nl>
2018-03-30 13:15:33 +0200

    Changes default rating to OBS. Removes rating from students table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('vatsim_id', 7)->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->unsignedInteger('rating_id')->default(3);
+            $table->unsignedInteger('rating_id')->default(1);
             $table->boolean('isAdmin')->default(0);
 
             $table->rememberToken();
