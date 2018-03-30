8ba115998635f5d255e2b72d4f491db52074f9c7
Dave Roverts <me@daveroverts.nl>
2018-03-30 14:59:13 +0200

    Adds a few mentor actions.
    I might add some more (Exams) later.

--- database/migrations/2018_03_30_111050_create_mentor_actions_table.php
+++ database/migrations/2018_03_30_111050_create_mentor_actions_table.php
@@ -16,7 +16,19 @@ class CreateMentorActionsTable extends Migration
         Schema::create('mentor_actions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('description');
         });
+
+        //Inserting all the types of actions that are available
+        $actions = [
+            ['name' => 'S1 mentor [DEL] [GND]',  'description' => "Person is a Student mentor, and is allowed to mentor Delivery [DEL], and Ground [GND] students"],
+            ['name' => 'S2 mentor [TWR]',  'description' => "Person is a Student 2 mentor, and is allowed to mentor Tower [TWR] students"],
+            ['name' => 'S3 mentor [APP]',  'description' => "Person is a Senior Student [S3] mentor, and is allowed to mentor Approach [APP] students"],
+            ['name' => 'C1 mentor [ACC]',  'description' => "Person is a Controller [C1] mentor, and is allowed to mentor Radar [ACC] students"],
+
+
+        ];
+        DB::table('actions')->insert($actions);
     }
 
     /**
