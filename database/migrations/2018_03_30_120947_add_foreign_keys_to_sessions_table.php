a7cce6a1f5ad4fa7d649cd9fe53bf60854d2dbe0
Dave Roverts <me@daveroverts.nl>
2018-03-30 14:11:02 +0200

    Renmavves sessions foreign keys migration

--- database/migrations/2018_03_30_120947_add_foreign_keys_to_sessions_table.php
+++ database/migrations/2018_03_30_120947_add_foreign_keys_to_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSessionsForeignKeys extends Migration
+class AddForeignKeysToSessionsTable extends Migration
 {
     /**
      * Run the migrations.
