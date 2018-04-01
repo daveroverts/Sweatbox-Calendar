a4aae4011a479c4a2195c5b1856d087085a687f4
Dave Roverts <me@daveroverts.nl>
2018-04-01 15:02:16 +0200

    Updates MentorController.create + store

--- resources/views/mentor/create.blade.php
+++ resources/views/mentor/create.blade.php
@@ -10,67 +10,22 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('mentor.store') }}">
                             @csrf
-                            {{--Vatsim ID--}}
+                            {{--Student--}}
                             <div class="form-group row">
-                                <label for="vatsim_id" class="col-md-4 col-form-label text-md-right"><i class="fa fa-user"></i> Vatsim ID</label>
+                                <label for="student" class="col-md-4 col-form-label text-md-right"><i class="fa fa-user"></i> Student</label>
 
                                 <div class="col-md-6">
-                                    <input id="vatsim_id" type="text" class="form-control{{ $errors->has('vatsim_id') ? ' is-invalid' : '' }}" name="vatsim_id" value="{{ old('vatsim_id') }}" required>
-
-                                    @if ($errors->has('vatsim_id'))
-                                        <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('vatsim_id') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            {{--Name--}}
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right"><i class="fa fa-user"></i> Name</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            {{--Email--}}
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right"><i class="fa fa-envelope"></i> E-Mail</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                    @if ($errors->has('email'))
-                                        <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            {{--Rating--}}
-                            <div class="form-group row">
-                                <label for="rating" class="col-md-4 col-form-label text-md-right"><i class="fa fa-list-ul"></i> Rating</label>
-
-                                <div class="col-md-6">
-                                    <select class="custom-select" name="rating">
-                                        @foreach($ratings as $rating)
-                                            <option name="rating" value="{{$rating->id}}">{{ $rating->longName }} [{{ $rating->shortName }}]</option>
+                                    <select class="custom-select" name="student">
+                                        @foreach($students as $student)
+                                            @if($student->user->rating_id >= 2)
+                                            <option name="student" value="{{$student->user->id}}">{{ $student->user->name }} [{{ $student->user->rating->shortName }}]</option>
+                                            @endif
                                         @endforeach
                                     </select>
 
-
-
-                                    @if ($errors->has('rating'))
+                                    @if ($errors->has('students'))
                                         <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('rating') }}</strong>
+                                        <strong>{{ $errors->first('student') }}</strong>
                                     </span>
                                     @endif
                                 </div>
