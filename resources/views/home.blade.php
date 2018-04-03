50cda3e51767bb668d49a92ba57885e115e2ade5
Dave Roverts <me@daveroverts.nl>
2018-04-03 12:02:26 +0200

    Adds missing letter in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 
                         <a href="calendar" class="btn btn-primary" role="button"><i class="fa fa-calendar"></i> Calendar Overview</a>
                         <a href="student" class="btn btn-primary" role="button"><i class="fa fa-users"></i> Students Overview</a>
-                        <a href="mentor" class="btn btn-primary" role="button"><i class="fa fa-users"></i> Mentor Overview</a>
+                        <a href="mentors" class="btn btn-primary" role="button"><i class="fa fa-users"></i> Mentors Overview</a>
                     </div>
                 </div>
             </div>
