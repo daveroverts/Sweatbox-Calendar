171fd6e32f1520bc75fc99fda7d14bc3e56c59f8
Dave Roverts <me@daveroverts.nl>
2018-04-05 09:54:06 +0200

    Adds bold in alert-title

--- resources/views/layouts/alert/danger.blade.php
+++ resources/views/layouts/alert/danger.blade.php
@@ -1,4 +1,4 @@
 <div class="alert alert-danger" role="alert">
-    <div class="alert-title">{{ $title }}</div>
+    <div class="alert-title"><b>{{ $title }}</b></div>
     {{ $slot }}
 </div>
--- resources/views/layouts/alert/light.blade.php
+++ resources/views/layouts/alert/light.blade.php
@@ -1,4 +1,4 @@
 <div class="alert alert-light" role="alert">
-    <div class="alert-title">{{ $title }}</div>
+    <div class="alert-title"><b>{{ $title }}</b></div>
     {{ $slot }}
 </div>
