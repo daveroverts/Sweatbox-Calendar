ad94315f26316874de8e4e1f85a358d3f99d3115
Dave Roverts <me@daveroverts.nl>
2018-04-06 09:02:50 +0200

    Changes one more formaction to the route() way. #14

--- resources/views/calendar/overview.blade.php
+++ resources/views/calendar/overview.blade.php
@@ -55,7 +55,7 @@
                                         @csrf
                                     </form></td>
 
-                                <td><form action="/calendar/{{$session->id}}/edit">
+                                <td><form action="{{ route('calendar.edit',$id) }}">
                                         <button class="btn btn-primary"><i class="fa fa-edit"></i> Edit Session</button>
                                         @csrf
                                     </form></td>
