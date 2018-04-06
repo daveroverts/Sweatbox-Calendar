03450f99e37bbc9000663166ff39424ffc68e01d
Dave Roverts <me@daveroverts.nl>
2018-04-06 13:54:24 +0200

    Replaces navigation buttons on home view with some text.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,7 @@
                             </div>
                         @endif
 
-                        <a href="calendar" class="btn btn-primary" role="button"><i class="fa fa-calendar"></i> Calendar Overview</a>
-                        <a href="student" class="btn btn-primary" role="button"><i class="fa fa-users"></i> Students Overview</a>
-                        <a href="mentor" class="btn btn-primary" role="button"><i class="fa fa-users"></i> Mentors Overview</a>
+                        <p>Welcome back, {{Auth::user()->name}}!</p>
                     </div>
                 </div>
             </div>
